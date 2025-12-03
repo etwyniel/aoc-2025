339bb0e79d1697fb2efb9fb704aadb29312115d6
Radiant Crab <radiant@crab.best>
2025-12-04 00:02:41 +0100

    Optimize day 2

--- src/day02.rs
+++ src/day02.rs
@@ -1,6 +1,6 @@
-use std::ops::RangeInclusive;
-use rayon::prelude::*;
+use std::{collections::HashSet, ops::RangeInclusive};
 
+use aoc_framework::bcd::Bcd;
 use aoc_framework::*;
 
 pub struct Day02;
@@ -9,7 +9,7 @@ impl_day!(Day02::{part1, part2}: 2025[2], r"
 11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
 ");
 
-fn parse(input: &str) -> impl Iterator<Item = RangeInclusive<u64>> {
+fn parse(input: &str) -> impl Iterator<Item = RangeInclusive<Bcd>> {
     input
         .split(',')
         .filter_map(|range| {
@@ -23,73 +23,77 @@ fn parse(input: &str) -> impl Iterator<Item = RangeInclusive<u64>> {
         .map(|(start, end)| start..=end)
 }
 
-fn decimal_len(mut n: u64) -> u32 {
-    let mut len = 0;
-    while n > 0 {
-        len += 1;
-        n /= 10;
+fn prefix(start: Bcd, end: Bcd) -> Bcd {
+    for i in (0..u64::BITS / 4).rev().skip(1) {
+        if start >> i != end >> i {
+            return start >> (i + 1);
+        }
     }
-    len
+    return start;
+}
+
+fn solve<const PART2: bool>(input: &str) -> u64 {
+    parse(input)
+        .map(|range| {
+            let start_len = range.start().len();
+            let end_len = range.end().len();
+            let res = (start_len..=end_len)
+                .map(|len| {
+                    let check_range = (Bcd::new(1) << (len - 1)).max(*range.start())
+                        ..=(Bcd::new(9).repeat(len).min(*range.end()));
+                    (len, check_range)
+                })
+                .map(|(len, check_range)| {
+                    let mut ids = HashSet::<u64>::new();
+                    let start = *check_range.start();
+                    let end = *check_range.end();
+                    let start_bin: u64 = start.into();
+                    let end_bin: u64 = end.into();
+
+                    let prefix = prefix(start, end);
+                    let prefix_len = prefix.len();
+
+                    let first_size = if PART2 { 1 } else { len / 2 };
+                    for size in first_size..=(len / 2) {
+                        if !(PART2 || len % 2 == 0) || len % size != 0 {
+                            continue;
+                        }
+
+                        let mask: u64 = Bcd::new(1).repeat_len(size, len / size).into();
+
+                        let remaining_digits = size.saturating_sub(prefix_len);
+                        let prefix_part: u64 = if prefix_len > size {
+                            prefix >> (prefix_len - size)
+                        } else {
+                            prefix << remaining_digits
+                        }
+                        .into();
+                        for n in 0..(10u32.pow(remaining_digits.max(0) as u32)) {
+                            let candidate = (prefix_part + n as u64) * mask;
+                            if candidate < start_bin {
+                                continue;
+                            }
+                            if candidate <= end_bin {
+                                ids.insert(candidate);
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                    ids.into_iter().sum::<u64>()
+                })
+                .sum::<u64>();
+            res
+        })
+        .sum()
 }
 
 #[aoc(part = 1, example = 1227775554)]
 fn part1(input: &str) -> u64 {
-    parse(input)
-        .collect::<Vec<_>>().into_par_iter()
-        .map(|range| {
-            let start_len = decimal_len(*range.start());
-            let end_len = decimal_len(*range.end());
-            (start_len..=end_len)
-                .filter(|len| len % 2 == 0)
-                .flat_map(|len| {
-                    let check_range = (10u64.pow(len - 1).max(*range.start()))
-                        ..(10u64.pow(len).min(*range.end() + 1));
-                    check_range.map(move |n| (len, n))
-                })
-                .filter(|&(len, n)| {
-                    let mask = 10u64.pow(len / 2);
-                    let half = n % mask;
-                    n == half + half * mask
-                })
-                .map(|(_, n)| n)
-                .sum::<u64>()
-        })
-        .sum()
+    solve::<false>(input)
 }
 
 #[aoc(part = 2, example = 4174379265)]
 fn part2(input: &str) -> u64 {
-    parse(input)
-        .collect::<Vec<_>>()
-        .into_par_iter()
-        .map(|range| {
-            let start_len = decimal_len(*range.start());
-            let end_len = decimal_len(*range.end());
-            (start_len..=end_len)
-                .flat_map(|len| {
-                    let check_range = (10u64.pow(len - 1).max(*range.start()))
-                        ..(10u64.pow(len).min(*range.end() + 1));
-                    check_range.map(move |n| (len, n))
-                })
-                .filter(|&(len, n)| {
-                    for hl in 1..=(len / 2) {
-                        if len % hl != 0 {
-                            continue;
-                        }
-                        let mask = 10u64.pow(hl);
-                        let part = n % mask;
-                        let mut res = 0;
-                        for _ in 0..(len / hl) {
-                            res = res * mask + part;
-                        }
-                        if n == res {
-                            return true;
-                        }
-                    }
-                    false
-                })
-                .map(|(_, n)| n)
-                .sum::<u64>()
-        })
-        .sum()
+    solve::<true>(input)
 }
