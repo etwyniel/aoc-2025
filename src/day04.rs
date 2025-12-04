1a23ed06af20388cfc079a5239a37f75bf193a9e
Radiant Crab <radiant@crab.best>
2025-12-04 18:58:04 +0100

    Optimize day 4

--- src/day04.rs
+++ src/day04.rs
@@ -1,4 +1,4 @@
-use aoc_framework::{grid::Grid, *};
+use aoc_framework::*;
 
 pub struct Day04;
 
@@ -15,14 +15,60 @@ impl_day!(Day04::{part1, part2}: 2025[4], r"
 @.@.@@@.@.
 ");
 
+fn neighbors(w: usize) -> [isize; 8] {
+    let mut points = [0; 8];
+    let mut n = 0;
+    for x in -1..=1 {
+        for y in -1..=1 {
+            if x == 0 && y == 0 {
+                continue;
+            }
+            points[n] = y * w as isize + x;
+            n += 1;
+        }
+    }
+    points
+}
+
+#[derive(PartialEq, Eq, Clone, Copy)]
+enum State {
+    Empty,
+    Present,
+    Checking,
+}
+
+fn parse(input: Vec<String>) -> (Vec<State>, Vec<usize>, [isize; 8]) {
+    let w = input[0].len() + 2;
+    let h = input.len() + 2;
+    let neighbors = neighbors(w);
+    let mut grid = vec![State::Empty; h * w];
+    let mut points = Vec::new();
+    input.iter().enumerate().for_each(|(y, ln)| {
+        let y = y + 1;
+        ln.bytes()
+            .enumerate()
+            .filter(|(_, b)| *b == b'@')
+            .for_each(|(x, _)| {
+                let x = x + 1;
+                let ndx = y * h + x;
+                grid[ndx] = State::Checking;
+                points.push(ndx);
+            })
+    });
+    (grid, points, neighbors)
+}
+
 #[aoc(part = 1, example = 13)]
 fn part1(input: Vec<String>) -> u64 {
-    let g = Grid::from_lines(input, |b| b == b'@');
-    g.points_iter()
-        .filter(|&p| g[p])
-        .filter(|p| {
-            p.neighbors_diag()
-                .filter(|&n| g.get(n) == Some(&true))
+    let (grid, points, neighbors) = parse(input);
+    points
+        .into_iter()
+        .filter(|&p| {
+            neighbors
+                .iter()
+                .map(|&off| (p as isize + off) as usize)
+                .filter(|&n| grid[n] != State::Empty)
+                .take(4)
                 .count()
                 < 4
         })
@@ -31,27 +77,32 @@ fn part1(input: Vec<String>) -> u64 {
 
 #[aoc(part = 2, example = 43)]
 fn part2(input: Vec<String>) -> u64 {
-    let mut g = Grid::from_lines(input, |b| b == b'@');
+    let (mut grid, mut stack, neighbors) = parse(input);
     let mut removed = 0;
-    loop {
-        let mut found = false;
-        for p in g.points_iter() {
-            if !g[p]
-                || p.neighbors_diag()
-                    .filter(|&n| g.get(n) == Some(&true))
-                    .take(4)
-                    .count()
-                    >= 4
-            {
+    let mut neigh = Vec::new();
+    'outer: while let Some(p) = stack.pop() {
+        grid[p] = State::Present;
+        neigh.clear();
+        for off in &neighbors {
+            let n = (p as isize + off) as usize;
+            if grid[n] == State::Empty {
                 continue;
             }
-            g.set(p, false);
-            removed += 1;
-            found = true
+            if neigh.len() >= 3 {
+                continue 'outer;
+            }
+            neigh.push(n);
         }
-        if !found {
-            break;
+
+        grid[p] = State::Empty;
+        for &n in neigh.iter() {
+            if grid[n] == State::Checking {
+                continue;
+            }
+            grid[n] = State::Checking;
+            stack.push(n);
         }
+        removed += 1;
     }
     removed
 }
